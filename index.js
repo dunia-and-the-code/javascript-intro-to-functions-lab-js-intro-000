3d13716c633138d7bba2f7d6991e9e29b13f6db8
Dunia Al-Baghdadi <dunia-al-baghdadi@hotmail.com>
2019-01-30 21:55:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string.toLowerCase()) {
-  return "I can'"
-  }
+  if (string === string.toLowerCase()) {
+    return "I can't hear you";
+  } else 
 }
